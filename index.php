356ff77c0d34cb532f43566eb7646b833123d0ba
adriansyska90 <37220827+adriansyska90@users.noreply.github.com>
2019-09-11 01:39:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Booking movie</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="img/icon/icon.png">
+  <!-- Place favicon.ico in the root directory -->
+  <link rel="shortcut icon" href="img/icon/icon.png">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <section class="slider-home">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="slider-home__wrap">
+            <h1>Co dzisiaj grają</h1>
+            <p class="mt-4">Szybki i wygodny sposób znajdź najbliższe kino oraz sprawdź jakie filmy w tym dniu grają</p>
+          </div>
+        </div>
+        <div class="col-lg-6 slider-home__image d-flex align-items-end">
+          <img src="./img/mobile-apk.png">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="info">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3"><h4>Założenia aplikacji</h4></div>
+        <div class="col-lg-9"><p>Zadaniem aplikacji jest znalezienie najbliższego kina w okolicy według lokalizatora, określenie ile czasu zajmie dotarcie na miejsce i wyświetlenie jego repertuaru. Dodatkową funkcją będzie możliwość dopasowania kilku filmów chcąc obejrzeć je tego samego dnia tak, by ograniczyć czas oczekiwania między seansami do minimum.</p></div>
+</div>
+</div>
+</div>
+</section>
+
+  </div>
+  
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="js/plugins.js"></script>
+</body>
+
+</html>
